6d328252c20107f2b09873f79e43a46628c3cff2
Dimitar Micevski <dmicevski@hotmail.com>
2020-11-20 09:06:50 -0500

    added project 1 info to portfolio page

--- portfolio.js
+++ portfolio.js
@@ -5,11 +5,6 @@ var modal3 = document.getElementById("myModal3");
 var modal4 = document.getElementById("myModal4");
 var modal5 = document.getElementById("myModal5");
 var modal6 = document.getElementById("myModal6");
-var modal7 = document.getElementById("myModal7");
-var modal8 = document.getElementById("myModal8");
-var modal9 = document.getElementById("myModal9");
-var modal10 = document.getElementById("myModal10");
-var modal11 = document.getElementById("myModal11");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -18,11 +13,6 @@ var span3 = document.getElementsByClassName("close3")[0];
 var span4 = document.getElementsByClassName("close4")[0];
 var span5 = document.getElementsByClassName("close5")[0];
 var span6 = document.getElementsByClassName("close6")[0];
-var span7 = document.getElementsByClassName("close7")[0];
-var span8 = document.getElementsByClassName("close8")[0];
-var span9 = document.getElementsByClassName("close9")[0];
-var span10 = document.getElementsByClassName("close10")[0];
-var span11 = document.getElementsByClassName("close11")[0];
 
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
@@ -42,21 +32,6 @@ var captionText5 = document.getElementById("caption5");
 var img6 = document.getElementById("myImg6");
 var modalImg6 = document.getElementById("img06");
 var captionText6 = document.getElementById("caption6");
-var img7 = document.getElementById("myImg7");
-var modalImg7 = document.getElementById("img07");
-var captionText7 = document.getElementById("caption7");
-var img8 = document.getElementById("myImg8");
-var modalImg8 = document.getElementById("img08");
-var captionText8 = document.getElementById("caption8");
-var img9 = document.getElementById("myImg9");
-var modalImg9 = document.getElementById("img09");
-var captionText9 = document.getElementById("caption9");
-var img10 = document.getElementById("myImg10");
-var modalImg10 = document.getElementById("img10");
-var captionText10 = document.getElementById("caption10");
-var img11 = document.getElementById("myImg11");
-var modalImg11 = document.getElementById("img11");
-var captionText11 = document.getElementById("caption11");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 
@@ -96,35 +71,6 @@ img6.onclick = function () {
     captionText6.innerHTML = this.alt;
 }
 
-img7.onclick = function () {
-    modal7.style.display = "block";
-    modalImg7.src = this.src;
-    captionText7.innerHTML = this.alt;
-}
-
-img8.onclick = function () {
-    modal8.style.display = "block";
-    modalImg8.src = this.src;
-    captionText8.innerHTML = this.alt;
-}
-
-img9.onclick = function () {
-    modal9.style.display = "block";
-    modalImg9.src = this.src;
-    captionText9.innerHTML = this.alt;
-}
-
-img10.onclick = function () {
-    modal10.style.display = "block";
-    modalImg10.src = this.src;
-    captionText10.innerHTML = this.alt;
-}
-
-img11.onclick = function () {
-    modal11.style.display = "block";
-    modalImg11.src = this.src;
-    captionText11.innerHTML = this.alt;
-}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
@@ -148,24 +94,4 @@ span5.onclick = function () {
 
 span6.onclick = function () {
     modal6.style.display = "none";
-}
-
-span7.onclick = function () {
-    modal7.style.display = "none";
-}
-
-span8.onclick = function () {
-    modal8.style.display = "none";
-}
-
-span9.onclick = function () {
-    modal9.style.display = "none";
-}
-
-span10.onclick = function () {
-    modal10.style.display = "none";
-}
-
-span11.onclick = function () {
-    modal11.style.display = "none";
 }
